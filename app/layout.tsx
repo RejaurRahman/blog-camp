0ebd0d2a137a9f02f51b0bdf9f08f4c96aaf44ee
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-10-02 00:25:54 +0100

    UPDATE: page directory and added app directory

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Blog Camp',
+  description: 'Blog Camp is a platform where I share thoughts about life topics covering wide range of aspects.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
