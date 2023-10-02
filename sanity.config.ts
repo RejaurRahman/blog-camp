e1d8cd91f504ea0ef57b5720ac6a73d42ab317b1
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-10-02 01:55:15 +0100

    ADD: theme to sanity to customize to blog camp colours

--- sanity.config.ts
+++ sanity.config.ts
@@ -9,19 +9,21 @@ import {deskTool} from "sanity/desk"
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from "./env"
 import {schema} from "./schema"
+import { blogCampTheme } from "./theme"
 
 export default defineConfig({
-  name: "BLOGCAMP_Content_Studio",
-  title: "BLOGCAMP Content Studio",
   basePath: "/studio",
-  projectId,
   dataset,
-  // Add and edit the content schema in the "./sanity/schema" folder
-  schema,
+  name: "BLOGCAMP_Content_Studio",
   plugins: [
     deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
   ],
+  projectId,
+  // Add and edit the content schema in the "./sanity/schema" folder
+  schema,
+  theme: blogCampTheme,
+  title: "BLOGCAMP Content Studio",
 })
