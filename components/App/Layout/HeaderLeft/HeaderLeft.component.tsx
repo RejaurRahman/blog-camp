b346ffd99f616f5deb595ce2e6a62a08af9ebb8c
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-10-03 00:56:42 +0100

    UPDATE: styles for header left content modal

--- components/App/Layout/HeaderLeft/HeaderLeft.component.tsx
+++ components/App/Layout/HeaderLeft/HeaderLeft.component.tsx
@@ -11,32 +11,47 @@ export default function HeaderLeft() {
       <form>
         <label>
           <input
-            className={`w-full ${styles.formField}`}
+            className={`w-full text-lg ${styles.formField}`}
             name="search"
             placeholder="Type here to search..."
             type="search"
           />
         </label>
       </form>
-      <span className={`flex justify-center items-center ${styles.title}`}>
+      <span className={`flex justify-center items-center text-3xl ${styles.title}`}>
         Blog Camp
       </span>
       <div className={`flex justify-between ${styles.bottom}`}>
-        <span className={styles.subtitle}>
+        <span className={`text-sm ${styles.subtitle}`}>
           Blog Camp is a platform where I share thoughts about life topics covering wide range of aspects.
         </span>
         <ul>
-          <li>
-            <Link href="/">Home</Link>
+          <li className="pb-6">
+            <Link
+              href="/"
+              className={`text-lg pb-6 ${styles.menuItem}`}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="pb-6">
+            <Link
+              href="/about"
+              className={`text-lg pb-6 ${styles.menuItem}`}
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
+            <Link
+              href="/contact"
+              className={`text-lg pb-6 ${styles.menuItem}`}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
-        <span className={styles.copyright}>
+        <span className={`text-sm uppercase ${styles.copyright}`}>
           Copyright &copy; {currentYear}. All rights reserved | Blog Camp
         </span>
       </div>
