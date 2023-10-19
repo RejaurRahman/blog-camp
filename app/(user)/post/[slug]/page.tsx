241430c892ed3909c1ac66d33f10fb3306ccc932
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-10-19 17:50:15 +0100

    ADD: blog header component for posts

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -5,6 +5,9 @@ import { client } from "@/lib/client";
 import urlFor from "@/lib/urlFor";
 
 import PostBanner from "@/components/App/PostBanner/PostBanner.component";
+import BlogHeader from "@/components/App/Blog/BlogHeader/BlogHeader.component";
+
+import styles from "./post.module.scss";
 
 type Props = {
   params: {
@@ -29,6 +32,9 @@ export default async function Post({ params: {slug} }: Props) {
         imageAltText={post.title}
         imageSrc={urlFor(post.mainImage).url()}
       />
+      <div className={styles.container}>
+        <BlogHeader post={post} />
+      </div>
     </article>
   )
 }
---
+++ components/App/Blog/BlogHeader/BlogHeader.component.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import blogListStyles from "../BlogList/BlogList.module.scss";
+import styles from "./BlogHeader.module.scss";
+
+type Props = {
+  post: Post;
+}
+
+export default function BlogHeader({ post }: Props) {
+  return (
+    <>
+      <div className="flex items-center pb-7">
+        <div
+          className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center"
+        >
+          {
+            post.categories?.map((category) => (
+              <div
+                className="bg-[#FFCC00] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
+                key={category._id}
+              >
+                <p>{category.title}</p>
+              </div>
+            ))
+          }
+        </div>
+        <p
+          className={`uppercase relative ml-2.5 ${blogListStyles.date}`}
+        >
+          <span className="ml-4">
+            {
+              new Date(post._createdAt).toLocaleDateString("en-US", {
+                day: "numeric",
+                month: "long",
+                year: "numeric"
+              })
+            }
+          </span>
+        </p>
+      </div>
+      <h1 className={`font-bold pb-5 ${styles.title}`}>
+        {post.title}
+      </h1>
+      <div className="mt-5 flex-1">
+        <p className={`text-gray-500 ${styles.description}`}>
+          {post.description}
+        </p>
+      </div>
+    </>
+  )
+}
